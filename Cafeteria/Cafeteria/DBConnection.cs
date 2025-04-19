5f0562094cb1d76e21ce35d4a773f5d8a3c27f21
tuankiet1803 <tuankiet18032004@gmail.com>
2025-04-19 08:27:58 +0700

    ket noi database

---
+++ Cafeteria/Cafeteria/DBConnection.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafeteria
+{
+    internal class DBConnection
+    {
+        SqlConnection sqlCon = new SqlConnection(Properties.Settings.Default.conn);
+        public void Execute(string command)
+        {
+            try
+            {
+                sqlCon.Open();
+                SqlCommand cmd = new SqlCommand(command, sqlCon);
+                if (cmd.ExecuteNonQuery() > 0)
+                {
+                    Console.WriteLine("Command executed successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("Command execution failed.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+        }
+        public DataTable Load(string command)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                sqlCon.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(command, sqlCon);
+                adapter.Fill(dt);
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                return null;
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+        }
+    }
+}
