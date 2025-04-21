68e5aec8971335296bc4faf6cd1277e03214188e
thienphucphan130@gmail.com <thienphucphan130@gmail.com>
2025-04-21 12:44:29 +0700

    login done

--- Cafeteria/Cafeteria/FLogin.cs
+++ Cafeteria/Cafeteria/FLogin.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,9 @@ namespace Cafeteria
 {
     public partial class FLogin : Form
     {
+        private DBConnection db;
+        public TextBox TxtUsername { get => txtUsername; set => txtUsername = value; }
+        public TextBox TxtPass { get => txtPass; set => txtPass = value; }
         public FLogin()
         {
             InitializeComponent();
@@ -24,9 +28,56 @@ namespace Cafeteria
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            FSale formSale = new FSale();
-            formSale.Show(); 
-            this.Hide();
+            string username = TxtUsername.Text;
+            string password = TxtPass.Text;
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Please enter username and password.");
+                return;
+            }
+
+            if (ValidateUserSales(username, password))
+            {
+
+                db = new DBConnection();
+
+                MessageBox.Show("Login successful!");
+
+                this.Hide();
+                FSale sale = new FSale();
+                sale.StartPosition = FormStartPosition.CenterScreen;
+                sale.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password.");
+            }
+        }
+        private bool ValidateUserSales(string username, string password)
+        {
+            using (SqlConnection con = new SqlConnection(Properties.Settings.Default.conn))
+            {
+                try
+                {
+                    con.Open();
+                    string sqlStr = "SELECT COUNT(*) FROM NhanVien WHERE Username = @username AND Password = @password AND Role = 'thungan'";
+                    SqlCommand cmd = new SqlCommand(sqlStr, con);
+                    cmd.Parameters.AddWithValue("@username", username);
+                    cmd.Parameters.AddWithValue("@password", password);
+                    int result = (int)cmd.ExecuteScalar();
+                    return result > 0;
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show("Error: " + exc.Message);
+                    return false;
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
         }
     }
 }
